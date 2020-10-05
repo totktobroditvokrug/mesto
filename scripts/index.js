eb8c9c256e3c2696c6144739398e3a906c3a051d
Stenin Sergey <totktobroditvokrug@yandex.ru>
2020-10-05 15:34:04 +0300

    исправить popup, добавить шрифт, изменить отклик заполнения формы

--- scripts/index.js
+++ scripts/index.js
@@ -4,41 +4,34 @@
 let buttonOpenPopup = document.querySelector('.button__edit');
 let buttonClosePopup = document.querySelector('.button__close');
 let popup = document.querySelector('.popup');
-function popupToggle() {
-    popup.classList.toggle('popup_on');
+let nameProfile = document.querySelector('.profile__name');  
+let jobProfile = document.querySelector('.profile__job');
+
+let formElement = document.querySelector('.popup');  // находим форму
+let nameInput = formElement.querySelector('#user-name'); // поле имени
+let jobInput = formElement.querySelector('#user-job');   // поле деятельности
+
+function popupOpen() {
+    nameInput.value = nameProfile.textContent;  // подгружаем значения профиля
+    jobInput.value = jobProfile.textContent;
+    popup.classList.add('popup_on');
 }
 
-buttonOpenPopup.addEventListener("click", popupToggle);
-buttonClosePopup.addEventListener("click", popupToggle);
+function popupClose() {
+    popup.classList.remove('popup_on');
+}
+
+buttonOpenPopup.addEventListener("click", popupOpen);
+buttonClosePopup.addEventListener("click", popupClose);
 
 
-let nameProfile = document.querySelector('.profile__name');
-let jobProfile = document.querySelector('.profile__job');
-console.log(nameProfile.textContent);
-
-// Находим форму в DOM
-
-let formElement = document.querySelector('.button__save');
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
 function formSubmitHandler (evt) {
-    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-                        // Так мы можем определить свою логику отправки.
-                        // О том, как это делать, расскажем позже.
-
-    // Находим поля формы в DOM
-    let nameInput = document.querySelector('#name-field').value;
-    let jobInput = document.querySelector('#job-field').value;
-
-    // Получите значение полей из свойства value
-
-    // Выберите элементы, куда должны быть вставлены значения полей
-
-    // Вставьте новые значения с помощью textContent
-    nameProfile.textContent = nameInput;
-    jobProfile.textContent = jobInput;
+    evt.preventDefault(); 
+ 
+    nameProfile.textContent = nameInput.value;
+    jobProfile.textContent = jobInput.value;
 }
 
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
-formElement.addEventListener('click', formSubmitHandler); 
+formElement.addEventListener('submit', formSubmitHandler);  // реагирует на enter без дополнительного кода
